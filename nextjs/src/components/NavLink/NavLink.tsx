7cb5478c44b5adbed63f9252e3b44e02f12f4dfc
Nikita <bananchik26@gmail.com>
2024-08-20 00:25:13 +0300

    sync

--- nextjs/src/components/NavLink/NavLink.tsx
+++ nextjs/src/components/NavLink/NavLink.tsx
@@ -4,11 +4,17 @@ import { NavLinkProps } from "./NavLink.props"
 import cn from 'classnames';
 import styles from './NavLink.module.scss';
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState, KeyboardEvent } from "react";
 
 
 export function NavLink({ href, children, dropData, className, ...props }: NavLinkProps) {
     const [dropFlag, setDropFlag] = useState(false);
+    const dropRef = useRef<HTMLUListElement>(null);
+
+    // useEffect(() => {
+    //     if (dropFlag)
+    //         dropRef.current?.tabIndex(1);
+    // }, [dropFlag]);
 
     const hoverHandle = () => {
         setDropFlag(true);
@@ -16,36 +22,53 @@ export function NavLink({ href, children, dropData, className, ...props }: NavLi
     const unhoverHandle = () => {
         setDropFlag(false);
     }
+    const keyHandle = (e: KeyboardEvent<HTMLAnchorElement>) => {
+        console.log(e.key);
+
+        if (e.key === "Tab") {
+            dropRef.current?.firstChild.focus();
+        }
+    }
 
     return (
-        <Link
-            href={href}
-            className={styles.link}
-            onMouseEnter={hoverHandle}
-            onMouseLeave={unhoverHandle}
-            {...props}
-        >
-            {children}
-            {!!dropData &&
-                <svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.5 2L6.5 7L1.5 2" stroke="black" strokeWidth="3" strokeLinecap="round"
-                        strokeLinejoin="round" />
-                </svg>
-            }
+        <>
+            <Link
+                href={href}
+                className={styles.link}
+                onMouseEnter={hoverHandle}
+                onMouseLeave={unhoverHandle}
+                onFocus={hoverHandle}
+                onBlur={unhoverHandle}
+                onKeyDown={(e: KeyboardEvent<HTMLAnchorElement>) => keyHandle(e)}
+                {...props}
+            >
+                {children}
+                {!!dropData &&
+                    <svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.5 2L6.5 7L1.5 2" stroke="black" strokeWidth="3" strokeLinecap="round"
+                            strokeLinejoin="round" />
+                    </svg>
+                }
+            </Link>
             {(!!dropData && dropFlag) &&
-                <div className={styles.dropMenu}>
+                <ul
+                    className={styles.dropMenu}
+                    ref={dropRef}
+                >
                     {dropData.map(item => {
                         return (
                             <Link
                                 key={`drop-item-${item.href}`}
                                 href={item.href}
+                                tabIndex={0}
+                                onFocus={hoverHandle}
                             >
                                 {item.text}
                             </Link>
                         )
                     })}
-                </div>
+                </ul>
             }
-        </Link>
+        </>
     )
 }
