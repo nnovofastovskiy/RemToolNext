129d197df9af406dd7dd0a1f10e02918164dc767
nnovofastovskiy <bananchik26@gmail.com>
2024-10-31 02:03:35 +0300

    priceparser is done

--- priceparser/pricelist.js
+++ priceparser/pricelist.js
@@ -1,32 +1,50 @@
-const fs = require('fs');
+import fs from "node:fs";
+import { parse } from 'node-html-parser';
 
-const regServe = /<div _ngcontent-crj-c34="" class="pricelist__serve">(.*?)<\/div>/g;
-const regCost = /<div _ngcontent-crj-c34="" class="pricelist__cost">(.*?)<\/div>/gi;
-const regTitle = /<h2.*?class="tool-page__title".*?>(.*?)<\/h2>/g;
-const regRow = /<div.*?class="pricelist__row".*?>(.*?)<\/div>/g;
-const regPTitle = /<h2.*?class="pricelist__title".*?>(.*?)<\/h2>/g;
+const regPTitle = /<h2 _ngcontent-.*?-c34="" class="tool-page__title">(.*?)<\/h2>/g;
+const regPrice = /<div _ngcontent-.{3}-c34="" class="tool-page__pricelist">/;
+const regBtn = /<button _ngcontent-.{3}-c34="" onclick=/;
 
-// fs.readdir('./src', (err, files) => {
-//     if (err) throw err;
-//     files.forEach(file => {
-//         let data = fs.readFileSync('./src/' + file, { encoding: 'utf8', flag: 'r' });
-//         // const services = Array.from(new Set([...data.matchAll(regex1)].map(item => item[1]))).slice(1);
-//         // const costs = Array.from(new Set([...data.matchAll(regex2)].map(item => item[1]))).slice(1);
-//         const rows = [...data.matchAll(regRow)];
-//         console.log(rows);
-//         const services = [...data.matchAll(regServe)].map(item => item[1]).slice(1);
-//         const costs = [...data.matchAll(regCost)].map(item => item[1]).slice(1);
-//         const result = services.reduce((acc, item, i) => {
-//             return acc += `${item}: ${costs[i]}\n`
-//         }, '');
-//         const title = [...data.matchAll(regTitle)][0][1];
-//         fs.writeFileSync(`./dist/${title}.txt`, result);
-//     })
-// });
+fs.readdir('./src', (err, files) => {
+    if (err) throw err;
+    files.forEach(filename => {
+        const file = fs.readFileSync(`./src/${filename}`, { encoding: 'utf8', flag: 'r' });
+        // console.log(file);
 
-const file = fs.readFileSync("./src/5fb048a05a7feb74d08e61a4.html", { encoding: 'utf8', flag: 'r' });
-const serves = [...file.matchAll(regServe)].map(item => item[1]).slice(1);
-const costs = [...file.matchAll(regCost)].map(item => item[1]).slice(1);
-console.log(serves);
+        const priceStart = file.search(regPrice);
+        const btnStart = file.search(regBtn);
+
+        const pageTitle = [...file.matchAll(regPTitle)][0][1];
+        console.log(pageTitle);
+
+        const priceStr = file.slice(priceStart, btnStart);
+
+        const cost = parse(priceStr);
+        const pricelist = [...cost.querySelectorAll('.pricelist__row')]
+            .slice(1)
+            .map(item => {
+                const title = item.querySelector('.pricelist__title');
+                if (title) return '### ' + title.innerText;
+                const serve = item.querySelector('.pricelist__serve');
+                const cost = item.querySelector('.pricelist__cost');
+                let row = '';
+                if (serve) {
+                    row += serve.innerText;
+                }
+                if (cost) {
+                    row += '; ' + cost.innerText;
+                }
+                return row;
+            })
+            .join('\n');
+
+        fs.writeFileSync(`./dist/${pageTitle}.txt`, pricelist);
+    })
+});
+
+
+// const serves = [...file.matchAll(regServe)].map(item => item[1]).slice(1);
+// const costs = [...file.matchAll(regCost)].map(item => item[1]).slice(1);
+// console.log(serves);
 
 
