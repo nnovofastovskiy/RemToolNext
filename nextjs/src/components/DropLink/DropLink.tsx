e966643cef8a263b553c89c7b53263f08e053e1c
Nikita <bananchik26@gmail.com>
2024-12-03 18:27:01 +0300

    dropLink in work

--- nextjs/src/components/DropLink/DropLink.tsx
+++ nextjs/src/components/DropLink/DropLink.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { DropLinkProps } from "./DropLink.props";
+import { DropData, DropLinkProps } from "./DropLink.props";
 import cn from 'classnames';
 import styles from './DropLink.module.scss';
 import Link from "next/link";
@@ -9,13 +9,18 @@ import { useEffect, useRef, useState, KeyboardEvent } from "react";
 
 export function DropLink({ href, children, dropData, className, ...props }: DropLinkProps) {
     const wrapperRef = useRef(null);
-
+    const [dropOpen, setDropOpen] = useState(false);
+    const [sideOpenItem, setSideOpenItem] = useState<DropData | null>(null);
 
     return (
         <ul
             ref={wrapperRef}
             className={styles.wrapper}
             {...props}
+            // onFocus={mouseOver}
+            // onBlur={mouseLeave}
+            onMouseOver={() => setDropOpen(true)}
+            onMouseLeave={() => setDropOpen(false)}
         >
             <li
                 className={styles.main_item}
@@ -23,14 +28,13 @@ export function DropLink({ href, children, dropData, className, ...props }: Drop
                 <Link
                     href={href}
                     className={cn(styles.link, styles.main_link)}
-                    onClick={(e) => {
-                        e.currentTarget.blur();
-                    }}
+                    onClick={() => setDropOpen(false)}
                 >
                     {children}
                 </Link>
-                <div
+                <button
                     className={styles.arrow}
+                    onClick={() => setDropOpen(!dropOpen)}
                 >
                     <svg
                         width="13"
@@ -42,27 +46,29 @@ export function DropLink({ href, children, dropData, className, ...props }: Drop
                         <path d="M11.5 2L6.5 7L1.5 2" stroke="black" strokeWidth="3" strokeLinecap="round"
                             strokeLinejoin="round" />
                     </svg>
-                </div>
-
-
+                </button>
             </li>
-            <ul className={styles.dropMenu}>
+            {dropOpen && <ul className={styles.dropMenu}>
                 {dropData?.map(item => {
                     return (
                         <li
+                            onMouseOver={() => setSideOpenItem(item)}
+                            onMouseLeave={() => setSideOpenItem(null)}
                             key={`drop-item-${item.href}`}
                             className={cn(styles.dropItem)}
                         >
                             <Link
                                 href={item.href}
                                 className={cn(styles.link, styles.first_drop_item)}
-                                onClick={(e) => {
-                                    e.currentTarget.blur();
-                                }}
+                                onClick={() => setDropOpen(false)}
                             >
                                 {item.text}
+                            </Link>
+                            <button
+                                onClick={() => sideOpenItem === item ? setSideOpenItem(null) : setSideOpenItem(item)}
+                                className={styles.arrow}
+                            >
                                 <svg
-                                    className={styles.arrow}
                                     width="13"
                                     height="9"
                                     viewBox="0 0 13 9"
@@ -72,30 +78,31 @@ export function DropLink({ href, children, dropData, className, ...props }: Drop
                                     <path d="M11.5 2L6.5 7L1.5 2" stroke="black" strokeWidth="3" strokeLinecap="round"
                                         strokeLinejoin="round" />
                                 </svg>
-                            </Link>
-                            <ul className={styles.sideDropMenu}>
-                                {item.dropData.map(item => {
-                                    return (
-                                        <li
-                                            key={`side-drop-item${item.href}`}
-                                        >
-                                            <Link
-                                                href={item.href}
-                                                className={cn(styles.link, styles.second_drop_item)}
-                                                onClick={(e) => {
-                                                    e.currentTarget.blur();
-                                                }}
+                            </button>
+                            {sideOpenItem &&
+                                <ul className={styles.sideDropMenu}>
+                                    {item.dropData.map(item => {
+                                        return (
+                                            <li
+                                                key={`side-drop-item${item.href}`}
                                             >
-                                                {item.text}
-                                            </Link>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
+                                                <Link
+                                                    href={item.href}
+                                                    className={cn(styles.link, styles.second_drop_item)}
+                                                    onClick={() => setDropOpen(false)}
+                                                >
+                                                    {item.text}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            }
                         </li>
                     )
                 })}
             </ul>
+            }
         </ul>
     )
 }
