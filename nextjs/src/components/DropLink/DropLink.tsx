4593cf2f2a24ad1bf93b7be341fc885a45fce484
nnovofastovskiy <bananchik26@gmail.com>
2024-12-05 18:57:49 +0300

    checkbox

--- nextjs/src/components/DropLink/DropLink.tsx
+++ nextjs/src/components/DropLink/DropLink.tsx
@@ -4,23 +4,19 @@ import { DropData, DropLinkProps } from "./DropLink.props";
 import cn from 'classnames';
 import styles from './DropLink.module.scss';
 import Link from "next/link";
-import { useEffect, useRef, useState, KeyboardEvent } from "react";
+import { useEffect, useRef, useState, KeyboardEvent, HTMLInputTypeAttribute, InputHTMLAttributes } from "react";
 
 
 export function DropLink({ href, children, dropData, className, ...props }: DropLinkProps) {
-    const wrapperRef = useRef(null);
-    const [dropOpen, setDropOpen] = useState(false);
-    const [sideOpenItem, setSideOpenItem] = useState<DropData | null>(null);
+    // const wrapperRef = useRef(null);
+    // const [dropOpen, setDropOpen] = useState(false);
+    // const [sideOpenItem, setSideOpenItem] = useState<DropData | null>(null);
 
     return (
         <ul
-            ref={wrapperRef}
             className={styles.wrapper}
             {...props}
-            // onFocus={mouseOver}
-            // onBlur={mouseLeave}
-            onMouseOver={() => setDropOpen(true)}
-            onMouseLeave={() => setDropOpen(false)}
+
         >
             <li
                 className={styles.main_item}
@@ -28,13 +24,19 @@ export function DropLink({ href, children, dropData, className, ...props }: Drop
                 <Link
                     href={href}
                     className={cn(styles.link, styles.main_link)}
-                    onClick={() => setDropOpen(false)}
                 >
                     {children}
                 </Link>
-                <button
+                <input
+                    className={styles.mainDropInput}
+                    type="checkbox"
+                    name="main-drop"
+                    id="main-drop"
+                    aria-label="Раскрыть меню"
+                />
+                <label
+                    htmlFor="main-drop"
                     className={styles.arrow}
-                    onClick={() => setDropOpen(!dropOpen)}
                 >
                     <svg
                         width="13"
@@ -46,26 +48,38 @@ export function DropLink({ href, children, dropData, className, ...props }: Drop
                         <path d="M11.5 2L6.5 7L1.5 2" stroke="black" strokeWidth="3" strokeLinecap="round"
                             strokeLinejoin="round" />
                     </svg>
-                </button>
+                </label>
             </li>
-            {dropOpen && <ul className={styles.dropMenu}>
+            <ul className={styles.dropMenu}>
                 {dropData?.map(item => {
                     return (
                         <li
-                            onMouseOver={() => setSideOpenItem(item)}
-                            onMouseLeave={() => setSideOpenItem(null)}
                             key={`drop-item-${item.href}`}
                             className={cn(styles.dropItem)}
                         >
                             <Link
                                 href={item.href}
                                 className={cn(styles.link, styles.first_drop_item)}
-                                onClick={() => setDropOpen(false)}
                             >
                                 {item.text}
                             </Link>
-                            <button
-                                onClick={() => sideOpenItem === item ? setSideOpenItem(null) : setSideOpenItem(item)}
+                            <input
+                                type="radio"
+                                name="sideDropInput"
+                                id={`sideDropInput-${item.href}`}
+                                className={styles.sideDropInput}
+                            // onClick={(e: React.MouseEvent<HTMLInputElement>) => {
+                            //     e.preventDefault();
+                            //     console.log(e.target.checked);
+                            //     if (e.target.checked) {
+                            //         e.target.checked = false;
+                            //     } else {
+                            //         e.target.checked = true;
+                            //     }
+                            // }}
+                            />
+                            <label
+                                htmlFor={`sideDropInput-${item.href}`}
                                 className={styles.arrow}
                             >
                                 <svg
@@ -78,31 +92,29 @@ export function DropLink({ href, children, dropData, className, ...props }: Drop
                                     <path d="M11.5 2L6.5 7L1.5 2" stroke="black" strokeWidth="3" strokeLinecap="round"
                                         strokeLinejoin="round" />
                                 </svg>
-                            </button>
-                            {sideOpenItem &&
-                                <ul className={styles.sideDropMenu}>
-                                    {item.dropData.map(item => {
-                                        return (
-                                            <li
-                                                key={`side-drop-item${item.href}`}
+                            </label>
+                            <ul className={styles.sideDropMenu}>
+                                {item.dropData.map(item => {
+                                    return (
+                                        <li
+                                            key={`side-drop-item${item.href}`}
+                                        >
+                                            <Link
+                                                href={item.href}
+                                                className={cn(styles.link, styles.second_drop_item)}
                                             >
-                                                <Link
-                                                    href={item.href}
-                                                    className={cn(styles.link, styles.second_drop_item)}
-                                                    onClick={() => setDropOpen(false)}
-                                                >
-                                                    {item.text}
-                                                </Link>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            }
+                                                {item.text}
+                                            </Link>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+
                         </li>
                     )
                 })}
             </ul>
-            }
+
         </ul>
     )
 }
